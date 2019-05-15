5349332c38c47c4c3cbd649b058715054db743c1
Daniel de Sales <ssales.daniel@gmail.com>
2019-05-14 21:42:51 -0300

    layout changed

--- frontend2/src/components/Login.vue
+++ frontend2/src/components/Login.vue
@@ -4,20 +4,17 @@
       <v-content>
         <v-container fluid fill-height>
           <v-layout align-center justify-center>
-            <v-flex xs12 sm8 md4>
+            <v-flex xs10 sm6 md3>
               <v-card class="elevation-12">
-                <v-toolbar dark color="primary">
-                  <v-toolbar-title>Login</v-toolbar-title>
-                </v-toolbar>
                 <v-card-text>
                   <v-form>
-                    <v-text-field v-model="user" prepend-icon="person" name="usuario" label="Usuário" type="text"></v-text-field>
+                    <v-text-field v-model="user" prepend-icon="person" name="usuario" label="User" type="text"></v-text-field>
                     <v-text-field v-model="password" prepend-icon="lock" name="password" label="Password" id="password" type="password"></v-text-field>
                   </v-form>
                 </v-card-text>
                 <v-card-actions>
                   <v-spacer></v-spacer>
-                  <v-btn v-on:click="loginAccess()" color="primary">Login</v-btn>
+                  <v-btn small v-on:click="loginAccess()" color="primary">Login</v-btn>
                 </v-card-actions>
               </v-card>
             </v-flex>
